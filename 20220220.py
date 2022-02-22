0de8bb7a95bbf4f63a946193ae2f6d79aa385b77
최수빈 (Choi Subin) <83692497+ssbinn@users.noreply.github.com>
2022-02-22 14:00:00 +0900

    Create 20220220.py

--- 20220220.py
+++ 20220220.py
@@ -6,25 +6,25 @@ import re
 
 from google_play_scraper import app
 
-browser = webdriver.Chrome("chromedriver.exe")
-browser.maximize_window()
+driver = webdriver.Chrome("chromedriver.exe")
+driver.maximize_window()
 
 url = "https://play.google.com/store/apps/collection/cluster?clp=0g4jCiEKG3RvcHNlbGxpbmdfZnJlZV9BUFBMSUNBVElPThAHGAM%3D:S:ANO1ljKs-KA&gsr=CibSDiMKIQobdG9wc2VsbGluZ19mcmVlX0FQUExJQ0FUSU9OEAcYAw%3D%3D:S:ANO1ljL40zU&hl=ko&gl=US"
-browser.get(url)
+driver.get(url)
 
-soup = BeautifulSoup(browser.page_source, 'lxml')
+soup = BeautifulSoup(driver.page_source, 'lxml')
 
 interval = 2
 
-page_height = browser.execute_script("return document.body.scrollHeight") # 페이지 높이 저장
+page_height = driver.execute_script("return document.body.scrollHeight") # 페이지 높이 저장
 # print(page_height)
 
 # 화면 맨 아래로 스크롤하도록 구현
 while True:
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)") # 현재 페이지에서 맨 아래로 스크롤 내리기 (아마 해상도 높이 만큼 내려갈걸)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") # 현재 페이지에서 맨 아래로 스크롤 내리기 (아마 해상도 높이 만큼 내려갈걸)
     time.sleep(interval) # 2초동안 페이지 로딩 대기
     
-    curr_height = browser.execute_script("return document.body.scrollHeight") # 현재 페이지 높이 저장
+    curr_height = driver.execute_script("return document.body.scrollHeight") # 현재 페이지 높이 저장
     if curr_height == page_height:
         break
     
