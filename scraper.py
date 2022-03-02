5b55ff85a1b6a4a015695dc57ceabac2fc6cfbb9
최수빈 (Choi Subin) <83692497+ssbinn@users.noreply.github.com>
2022-03-02 18:37:11 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,49 +5,55 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import re
-
+import pandas as pd
 from google_play_scraper import app
 
-driver = webdriver.Chrome("chromedriver.exe")
-driver.maximize_window()
 
 url = "https://play.google.com/store/apps/collection/cluster?clp=0g4jCiEKG3RvcHNlbGxpbmdfZnJlZV9BUFBMSUNBVElPThAHGAM%3D:S:ANO1ljKs-KA&gsr=CibSDiMKIQobdG9wc2VsbGluZ19mcmVlX0FQUExJQ0FUSU9OEAcYAw%3D%3D:S:ANO1ljL40zU&hl=ko&gl=US"
+
+# ---스크롤 함수---
+interval = 0.8
+def scrolling():
+    try:
+        last_height = driver.execute_script("return document.body.scrollHeight") # 페이지 높이 저장
+        
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") # 현재 페이지에서 맨 아래로(해상도 높이만큼) 스크롤 내리기
+            time.sleep(interval) # 2초동안 페이지 로딩 대기
+
+            curr_height = driver.execute_script("return document.body.scrollHeight") # 현재 페이지 높이 저장
+            if curr_height == last_height:
+                break
+
+            last_height = curr_height
+
+    except Exception as e:
+        print("error occurred: ", e)
+
+        
+# ---HTML 데이터 가져오기---
+driver = webdriver.Chrome("chromedriver.exe")
+driver.maximize_window()
 driver.get(url)
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
+scrolling() # 페이지 맨 아래로 스크롤
+soup = BeautifulSoup(driver.page_source, 'html.parser') # HTML 파싱하기
 
-interval = 2
-
-page_height = driver.execute_script("return document.body.scrollHeight") # 페이지 높이 저장
-# print(page_height)
-
-# 화면 맨 아래로 스크롤하도록 구현
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") # 현재 페이지에서 맨 아래로 스크롤 내리기 (아마 해상도 높이 만큼 내려갈걸)
-    time.sleep(interval) # 2초동안 페이지 로딩 대기
-    
-    curr_height = driver.execute_script("return document.body.scrollHeight") # 현재 페이지 높이 저장
-    if curr_height == page_height:
-        break
-    
-    page_height = curr_height
-
+apps = soup.find_all("div", attrs={"class":"b8cIId ReQCgd Q9MA7b"}) # 이 div 태그 아래에 있는 a 태그를 읽어오기 위함
+# print(len(apps)) # 200개 모두 출력됨 
 
 app_id_list = [] 
 
-# apps_title = soup.find_all("div", attrs={"class":"b8cIId ReQCgd Q9MA7b"})
-apps_title = soup.select("div.b8cIId.ReQCgd.Q9MA7b > a")
+for i in apps:
+    link = i.find("a")["href"]
+    app_id_list.append(link[23:])
 
-for i in apps_title:
-    id = i.attrs["href"]
-    app_id_list.append(id[23:])
-    
-# print(app_id_list)
+start = time.time()
 
-for j in app_id_list:
-    
-    result = app(j,
-        lang='ko'
-    )
+# us, id 뽑아온 이유 설명 추가하기
+for app_id in app_id_list:
+    result = app(app_id,
+        lang='ko',
+        country='us'
+    ) 
     print(result)
-
